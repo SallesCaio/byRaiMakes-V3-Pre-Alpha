1bfbfa556232b9db92f3f03dba4121d1c4dcb1e3
SallesCaio <sallescaiosilva@gmail.com>
2026-07-30 21:07:14 -0300

    config: Firebase config para raimake-v3 (dev + prod)

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: "AIzaSyDrSQUYGptywpcVbQrpH4LtyCgnnmS8r9k",
-    authDomain: "byraimake.firebaseapp.com",
-    projectId: "byraimake",
-    storageBucket: "byraimake.firebasestorage.app",
-    messagingSenderId: "651824112620",
-    appId: "1:651824112620:web:b7719eaf89b6193efd96c9"
+    apiKey: "AIzaSyCJtWo6cSPmeFUSMzVudvET3MoMExVoT3k",
+    authDomain: "raimake-v3.firebaseapp.com",
+    projectId: "raimake-v3",
+    storageBucket: "raimake-v3.firebasestorage.app",
+    messagingSenderId: "333702490931",
+    appId: "1:333702490931:web:eecc919a29a8cc464b6c46"
   }
 };
